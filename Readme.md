757e7a3b6492200a069c2a22ff7c4380c70294c5
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-10-29 03:33:27 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+используемый стек:
+Java 11, Spring 2.5.6 (Boot, Web, Test), Gradle
---
+++ src/main/java/ru/xodavit/simpletelegrambot/SimpleTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.xodavit.simpletelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTelegramBotApplication.class, args);
+	}
+
+}
